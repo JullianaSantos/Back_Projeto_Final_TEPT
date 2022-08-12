ef128e07425a7c9af8d401fc45dde5aad702899c
Julliana Santos <104655914+DevJulliana@users.noreply.github.com>
2022-08-12 13:30:37 -0300

    ajustando configurações de email

--- src/modules/user_mailer.js
+++ src/modules/user_mailer.js
@@ -14,10 +14,10 @@ function sendMail(transporter, to, token) {
   const mailData = {
     from: process.env.PROJECT_EMAIL,
     to: to,
-    subject: "Mensagem, clique aqui!",
+    subject: "Para concluir o processo de redefinir senha, clique aqui!",
     text: "Texto!",
     html:
-      "<a href='http://localhost:8080/user/" + token + "'>Mudar senha</a>",
+      "<a href='https://institutotept.herokuapp.com/user/" + token + "'>Mudar senha</a>",
   };
 
   transporter.sendMail(mailData);
